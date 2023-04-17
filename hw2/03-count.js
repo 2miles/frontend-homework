3f7d788427f5cddcf3a09da6fd27fd6326593100
miles <miles4599@gmail.com>
2023-04-17 02:12:57 -0700

    Finished findWord(). Used to locate a the word entered into textBox

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,31 @@
-// Add your code here
+/* eslint-disable no-console */
 
-input.addEventListener('keydown', handleKeyDown);
+const inputBox = document.querySelector('input');
+const paragraphText = document.querySelector('#paragraph').textContent;
+
+/**
+ * Returns false if any characters of `word` are non-alphanumeric
+ */
+const isValid = function isValid(word) {
+  const re = /^[A-z0-9]*$/;
+  return re.test(word);
+};
+
+/**
+ * Returns the location of `word` in `text`.
+ * If word is not found returns -1.
+ */
+const findWord = function findWord(word, text) {
+  if (!isValid(word)) return -1;
+  const pattern = ` ${word}[^A-z]`;
+  const re = new RegExp(pattern);
+  return text.search(re);
+};
+
+const handleInput = function handleInput() {
+  const result = findWord(inputBox.value, paragraphText);
+  console.log(result);
+};
+
+// inputBox.addEventListener('keydown', handleKeyDown);
+inputBox.addEventListener('input', handleInput);
