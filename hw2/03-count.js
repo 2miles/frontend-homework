b4f8dbce569900af2e5478dba4996f4811861e6a
miles <miles4599@gmail.com>
2023-04-17 21:51:11 -0700

    refactored count.js a bit

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -4,51 +4,36 @@ const inputBox = document.querySelector('input');
 const oldText = document.querySelector('#paragraph').textContent;
 
 /**
- * Returns false if any characters of `word` are non-alphanumeric
+ * Returns false if `word` is actually multiple words.
  */
-const isValid = function isValid(word) {
-  const re = /^[A-z0-9]*$/;
-  return re.test(word);
+const isOneWord = function isOneWord(word) {
+  const trimmedWord = word.trim();
+  const re = /^[^ ]*$/;
+  return re.test(trimmedWord);
 };
 
 /**
- * Returns the location of `word` in `text`.
- * If word is not found returns -1.
+ * Returns false if `word` is actually multiple words.
  */
+const replace = function replace(text, pattern, replacement) {
+  const re = new RegExp(pattern, 'g');
+  return text.replace(re, replacement);
+};
+
 const highlightWords = function highlightWords() {
-  const word = inputBox.value;
-  let newText = oldText;
-  if (!isValid(word)) {
+  let text = oldText;
+  const word = inputBox.value.trim();
+  if (isOneWord(word) === false) {
     document.getElementById('paragraph').innerHTML = oldText;
     return;
   }
-  if (word === '') return;
+  text = replace(text, `[\\s]${word}[\\s]`, ` <span>${word}</span> `);
+  text = replace(text, `[\\s]${word}[,]`, ` <span>${word}</span>,`);
+  text = replace(text, `[\\s]${word}[.]`, ` <span>${word}</span>.`);
+  text = replace(text, `[\\s]${word}[—]`, ` <span>${word}</span>—`);
+  text = replace(text, `[—]${word}[\\s]`, `—<span>${word}</span> `);
 
-  const pattern1 = `[\\s]${word}[\\s]`;
-  const re1 = new RegExp(pattern1, 'g');
-  console.log(oldText);
-  newText = newText.replace(re1, ` <span class="highlight">${word}</span> `);
-
-  const pattern2 = `[\\s]${word}[,]`;
-  const re2 = new RegExp(pattern2, 'g');
-  console.log(oldText);
-  newText = newText.replace(re2, ` <span class="highlight">${word}</span>,`);
-
-  const pattern3 = `[\\s]${word}[.]`;
-  const re3 = new RegExp(pattern3, 'g');
-  console.log(oldText);
-  newText = newText.replace(re3, ` <span class="highlight">${word}</span>.`);
-
-  const pattern4 = `[\\s]${word}[—]`;
-  const re4 = new RegExp(pattern4, 'g');
-  console.log(oldText);
-  newText = newText.replace(re4, ` <span class="highlight">${word}</span>—`);
-
-  const pattern5 = `[—]${word}[\\s]`;
-  const re5 = new RegExp(pattern5, 'g');
-  console.log(oldText);
-  newText = newText.replace(re5, `—<span class="highlight">${word}</span> `);
-  document.getElementById('paragraph').innerHTML = newText;
+  document.getElementById('paragraph').innerHTML = text;
 };
 
 const handleInput = function handleInput() {
