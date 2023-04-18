1938b96d2931a824258dbf10dc0b60c7d510f757
miles <miles4599@gmail.com>
2023-04-17 20:41:19 -0700

    highlighting working correctly

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 
 const inputBox = document.querySelector('input');
-const paragraphText = document.querySelector('#paragraph').textContent;
+const oldText = document.querySelector('#paragraph').textContent;
 
 /**
  * Returns false if any characters of `word` are non-alphanumeric
@@ -15,30 +15,44 @@ const isValid = function isValid(word) {
  * Returns the location of `word` in `text`.
  * If word is not found returns -1.
  */
-const findWord = function findWord(word, text) {
-  if (!isValid(word)) return -1;
-  const pattern = ` ${word}[^A-z]`;
-  const re = new RegExp(pattern);
-  return text.search(re);
-};
+const highlightWords = function highlightWords() {
+  const word = inputBox.value;
+  let newText = oldText;
+  if (!isValid(word)) {
+    document.getElementById('paragraph').innerHTML = oldText;
+    return;
+  }
+  if (word === '') return;
 
-const clearDivText = function clearDivText(elementID) {
-  document.getElementById(elementID).innerHTML = '';
+  const pattern1 = `[\\s]${word}[\\s]`;
+  const re1 = new RegExp(pattern1, 'g');
+  console.log(oldText);
+  newText = newText.replace(re1, ` <span class="highlight">${word}</span> `);
+
+  const pattern2 = `[\\s]${word}[,]`;
+  const re2 = new RegExp(pattern2, 'g');
+  console.log(oldText);
+  newText = newText.replace(re2, ` <span class="highlight">${word}</span>,`);
+
+  const pattern3 = `[\\s]${word}[.]`;
+  const re3 = new RegExp(pattern3, 'g');
+  console.log(oldText);
+  newText = newText.replace(re3, ` <span class="highlight">${word}</span>.`);
+
+  const pattern4 = `[\\s]${word}[—]`;
+  const re4 = new RegExp(pattern4, 'g');
+  console.log(oldText);
+  newText = newText.replace(re4, ` <span class="highlight">${word}</span>—`);
+
+  const pattern5 = `[—]${word}[\\s]`;
+  const re5 = new RegExp(pattern5, 'g');
+  console.log(oldText);
+  newText = newText.replace(re5, `—<span class="highlight">${word}</span> `);
+  document.getElementById('paragraph').innerHTML = newText;
 };
 
 const handleInput = function handleInput() {
-  const paragraph = document.getElementById('paragraph');
-  clearDivText('paragraph');
-  const loc = findWord(inputBox.value, paragraphText);
-  paragraph.innerHTML = paragraphText.substring(0, loc + 1);
-  const spanElem = document.createElement('span');
-  const len = inputBox.value.length;
-  const start = loc + 1;
-  const end = start + len;
-  spanElem.innerHTML = paragraphText.substring(start, end);
-  spanElem.style.backgroundColor = 'yellow';
-  paragraph.append(spanElem);
-  paragraph.append(paragraphText.substring(end, paragraphText.length));
+  highlightWords();
 };
 
 // inputBox.addEventListener('keydown', handleKeyDown);
