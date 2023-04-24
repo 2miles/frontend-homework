4907391134f2da00c6b6b115d817991836aa0cc0
miles <miles4599@gmail.com>
2023-04-24 16:12:51 -0700

    Ex-4 finished

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,5 +1,4 @@
-const body = document.querySelector('body');
-
+let interval = 3000;
 /**
  * Returns a random integer between 0 and `max` (exclusive)
  */
@@ -8,7 +7,7 @@ function getRandomInt(max) {
 }
 
 /**
- * Returns a random hsla color string
+ * Returns a random hsla color string with a 0.5 alpha value
  */
 const buildColor = function buildHslaColor() {
   const h = getRandomInt(360 + 1).toString();
@@ -17,25 +16,46 @@ const buildColor = function buildHslaColor() {
   return `hsla(${h}, ${s}%, ${l}%, 0.5)`;
 };
 
-const setBGColor = function setBGColor() {
+const setRandomBGColor = function setBGColor() {
+  const body = document.querySelector('body');
   body.style.backgroundColor = buildColor();
 };
 
-// Add your code here
-window.addEventListener('load', () => {
-  setBGColor();
-});
+/**
+ * Set the background to a random color when the page loads
+ */
+window.addEventListener('load', setRandomBGColor);
 
-let intervalID = null;
+const inputBox = document.querySelector('#user-input');
+
+/**
+ * Parse `inputBox` value string as a positive integer representing number
+ * of seconds. Return that amount in milliseconds. If input is not a positive
+ * integer, return 3000.
+ */
+const parseTextBox = function parseTextBox() {
+  const inputAsInt = parseInt(inputBox.value, 10);
+  if (inputAsInt < 1 || Number.isNaN(inputAsInt)) return 3000;
+  return inputAsInt * 1000;
+};
+
+const updateInterval = function updateInterval() {
+  interval = parseTextBox();
+};
+
+inputBox.addEventListener('input', updateInterval);
+
 const btn = document.querySelector('#button');
-btn.addEventListener('click', () => {
+let intervalID = null;
+
+const handleButtonEvent = function handleButtonEvent() {
   if (btn.classList.contains('start')) {
     btn.classList.remove('start');
     btn.classList.add('stop');
     btn.classList.remove('btn-primary');
     btn.classList.add('btn-danger');
     btn.textContent = 'Stop';
-    intervalID = setInterval(setBGColor, 3000);
+    intervalID = setInterval(setRandomBGColor, interval);
   } else {
     btn.classList.remove('stop');
     btn.classList.add('start');
@@ -45,4 +65,6 @@ btn.addEventListener('click', () => {
     clearInterval(intervalID);
     intervalID = null;
   }
-});
+};
+
+btn.addEventListener('click', handleButtonEvent);
