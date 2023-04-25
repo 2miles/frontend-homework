53e4dc668b15062f4cb74d1db083033c71b330ea
miles <miles4599@gmail.com>
2023-04-25 00:22:14 -0700

    Finished Ex5

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,36 @@
-// Add your code here
+/* eslint-disable no-console */
+
+const form = document.querySelector('#form');
+
+const buildClassesStr = function buildClassesStr(boxes) {
+  const classes = [];
+  boxes.forEach((i) => {
+    classes.push(i.value);
+  });
+  let classesStr = 'None';
+  if (classes.length > 0) {
+    classesStr = classes.join(', ');
+  }
+  return classesStr;
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const fullName = document.querySelector('#full_name').value;
+  const email = document.querySelector('#email').value;
+  const reg = document.querySelector('#registration');
+  const regStr = reg.options[reg.selectedIndex].text;
+  const checkBoxes = document.querySelectorAll('input[name=courses]:checked');
+  const textArea = document.querySelector('#anything_else');
+  let anythingElse = 'n/a';
+  if (textArea.value) {
+    anythingElse = textArea.value;
+  }
+
+  console.log(`\
+  FULL NAME:      ${fullName}\n\
+  EMAIL:          ${email}\n\
+  REGISTRATION :  ${regStr}\n\
+  CLASSES TAKEN:  ${buildClassesStr(checkBoxes)}\n\
+  ANYTHING ELSE:  ${anythingElse}`);
+});
