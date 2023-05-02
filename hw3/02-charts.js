d80c46fe73de880f73a2309a602958ca640a186d
miles <miles4599@gmail.com>
2023-05-02 14:48:49 -0700

    Get family names and remove typos.

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -26,12 +26,57 @@ const borderColors = [
   'rgba(78, 52, 199, 1)',
 ];
 
+/**
+ * Removes typos and sorts the the GOT family name array.
+ */
+const cleanNames = function cleanNames(familyArr) {
+  const families = familyArr;
+  familyArr.forEach((e, index) => {
+    let newE = e;
+    if (e.includes('House')) {
+      newE = e.split(' ').slice(1).join(' ');
+      families[index] = newE;
+    }
+    if (e === '') {
+      newE = 'None';
+    } else if (e.includes('Targar')) {
+      newE = 'Targaryen';
+    } else if (e.includes('Unk')) {
+      newE = 'Unknown';
+    } else if (e.includes('Lan')) {
+      newE = 'Lannister';
+    } else if (e.includes('Lor')) {
+      newE = 'Lorath';
+    }
+    families[index] = newE;
+  });
+  return families.sort();
+};
+
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    // const characters = data;
+    // characters.forEach((character) => {
+    // });
+    const gotData = data;
+    const families = [];
+    gotData.forEach((character) => {
+      families.push(character.family);
+    });
+    cleanNames(families);
+    console.log(families);
+  })
+  .catch((err) => console.log(err));
 
 const renderChart = () => {
   const donutChart = document.querySelector('.donut-chart');
 
+  // eslint-disable-next-line no-new, no-undef
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
