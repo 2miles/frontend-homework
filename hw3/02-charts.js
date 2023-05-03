86640d55e08a5a5c8509e098191d5815373e56c3
miles <miles4599@gmail.com>
2023-05-02 19:15:18 -0700

    correct data is now being displayed

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -25,70 +25,62 @@ const borderColors = [
   'rgba(210, 199, 199, 1)',
   'rgba(78, 52, 199, 1)',
 ];
-
 /**
- * Removes typos and sorts the the GOT family name array.
+ * Uses array destructuring and the Set constructor to remove duplicates
  */
-const cleanFamilyNames = function cleanFamilyNames(familyNames) {
+const removeDups = function removeDups(arr) {
+  return [...new Set(arr)];
+};
+/**
+ * Removes typos and removes everyone who is not in a house.
+ */
+const cleanNames = function cleanNames(familyNames) {
   const names = familyNames;
+  const goodNames = [];
   names.forEach((name, index) => {
     let goodName = name;
-    if (name.includes('House')) {
-      goodName = name.split(' ').slice(1).join(' ');
-      names[index] = goodName;
+    if (name.includes('Targar')) {
+      goodName = 'House Targaryen';
     }
-    if (name === '') {
-      goodName = 'None';
-    } else if (name.includes('Targar')) {
-      goodName = 'Targaryen';
-    } else if (name.includes('Unk')) {
-      goodName = 'Unknown';
-    } else if (name.includes('Lan')) {
-      goodName = 'Lannister';
-    } else if (name.includes('Lor')) {
-      goodName = 'Lorath';
+    if (name.includes('Lan')) {
+      goodName = 'House Lannister';
+    } else if (name.includes('Stark')) {
+      goodName = 'House Stark';
+    } else if (name.includes('Bara')) {
+      goodName = 'House Baratheon';
+    } else if (name.includes('Grey')) {
+      goodName = 'House Greyjoy';
+    } else if (name.includes('Tyrell')) {
+      goodName = 'House Tyrell';
     }
     names[index] = goodName;
   });
-  return names.sort();
-};
-
-/**
- * Uses array destructuring and the set constructor to remove duplicates
- */
-const buildNameList = function buildNameList(families) {
-  return [...new Set(families)];
+  names.forEach((name) => {
+    if (name.includes('House')) {
+      goodNames.push(name);
+    }
+  });
+  goodNames.sort();
+  return goodNames;
 };
 
 /**
  * Return a list of counts of people with each name.
  */
-const buildCountList = function buildCountList(people, names) {
+const buildCountList = function buildCountList(people, houses) {
   const counts = [];
-  names.forEach((name, nameIndex) => {
-    counts[nameIndex] = 0;
+  houses.forEach((house, index) => {
+    counts[index] = 0;
     people.forEach((person) => {
-      if (person === name) {
-        counts[nameIndex] += 1;
+      if (person === house) {
+        counts[index] += 1;
       }
     });
   });
   return counts;
 };
 
-/**
- * Display the names and the counts.
- * For debugging.
- */
-const displayCounts = function display(names, count) {
-  names.forEach((name, index) => {
-    console.log('Name: ', name, ', Count', count[index]);
-  });
-};
-
-// url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
-let names = [];
+let houses = [];
 let counts = [];
 
 const renderChart = () => {
@@ -97,7 +89,7 @@ const renderChart = () => {
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: names,
+      labels: houses,
       datasets: [
         {
           label: 'My First Dataset',
@@ -111,20 +103,19 @@ const renderChart = () => {
   });
 };
 
+// url for the Thrones API
+const url = 'https://thronesapi.com/api/v2/Characters';
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
     const gotData = data;
-    console.log(data);
-    const people = [];
+    let people = [];
     gotData.forEach((person) => {
       people.push(person.family);
     });
-    // cleanFamilyNames(people);
-    console.log(people);
-    names = buildNameList(people);
-    counts = buildCountList(people, names);
-    displayCounts(names, counts);
+    people = cleanNames(people);
+    houses = removeDups(people);
+    counts = buildCountList(people, houses);
   })
   .then(renderChart)
   .catch((err) => console.log(err));
