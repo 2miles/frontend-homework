1ee349966e71349b1b7a69c90f32b1da61ef9d0c
miles <miles4599@gmail.com>
2023-05-15 22:27:18 -0700

    houses.js now uses useFetch custom hook and displays message if pending or error

--- hw3/Ex-03-create-react-app-vscode/src/views/Houses.js
+++ hw3/Ex-03-create-react-app-vscode/src/views/Houses.js
@@ -1,5 +1,5 @@
 import 'chart.js/auto';
-import axios from 'axios';
+import useFetch from '../hooks/useFetch';
 import { useEffect, useState } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import { backgroundColors, borderColors } from '../utils/chartColors';
@@ -8,42 +8,41 @@ import { cleanNames, removeDupes, buildCountList } from '../utils/housesUtils';
 const url = 'https://thronesapi.com/api/v2/Characters';
 
 export default function Houses() {
+  const { data: characters, isPending, error } = useFetch(url);
   const [doughnutData, setDoughnutData] = useState({});
 
   useEffect(() => {
-    axios
-      .get(url)
-      .then((res) => {
-        const families = [];
-        res.data.forEach((d) => {
-          families.push(d.family);
-        });
-        const houseNames = cleanNames(families);
-        const houseNameLabels = removeDupes(houseNames);
-        const houseNameCounts = buildCountList(houseNames, houseNameLabels);
-        setDoughnutData({
-          labels: houseNameLabels,
-          datasets: [
-            {
-              // label: houseNameLabels,
-              data: houseNameCounts,
-              backgroundColor: backgroundColors,
-              borderColor: borderColors,
-              borderWidth: 2,
-            },
-          ],
-        });
-      })
-      /* eslint-disable no-console */
-      .catch((err) => console.log(err));
-  }, []);
+    const families = [];
+    characters.forEach((d) => {
+      families.push(d.family);
+    });
+    const houseNames = cleanNames(families);
+    const houseNameLabels = removeDupes(houseNames);
+    const houseNameCounts = buildCountList(houseNames, houseNameLabels);
+    setDoughnutData({
+      labels: houseNameLabels,
+      datasets: [
+        {
+          data: houseNameCounts,
+          backgroundColor: backgroundColors,
+          borderColor: borderColors,
+          borderWidth: 2,
+        },
+      ],
+    });
+  }, [characters]);
 
-  if (Object.keys(doughnutData).length === 0)
+  if (isPending) {
+    return <h2>Loading Data...</h2>;
+  }
+  if (error) {
     return (
-      <div className="container">
-        <h1>Houses</h1>
+      <div>
+        <h2>Error Loading Data</h2>
+        <p>{error}</p>
       </div>
     );
+  }
   return (
     <div className="container d-flex flex-column align-items-center">
       <h1>Houses</h1>
